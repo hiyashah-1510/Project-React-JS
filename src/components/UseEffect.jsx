dea5afe1e70049e67dc4a4086f370aaecbb3448f
Hiya Shah <hiyashah006@gmail.com>
2025-11-25 12:11:48 +0530

    Enhance UseEffect component with lifecycle logging
    Added console logs for component lifecycle events.

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-function UseEffect(){
-    const [time , setTime] = useState(new Date)
+function UseEffect() {
+  const [time, setTime] = useState(new Date());
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(new Date)
-        }, 1000);
+  useEffect(() => {
+    console.log("Component Mounted");
 
-        return() => clearInterval(Date)
-    },[])
+    const timer = setInterval(() => {
+      setTime(new Date());
+      console.log("🔄 Component Updated");
+    }, 1000);
 
+    return () => {
+      clearInterval(timer);
+      console.log("Component Unmounted");
+    };
+  }, []);
 
-    return(
-        <>
-            <h1>Real Time example</h1>
-            <h3>Time : {time.toLocaleTimeString()}</h3>
-        </>
-    )
+  return (
+    <>
+      <h1>Real Time Example</h1>
+      <h3>Time: {time.toLocaleTimeString()}</h3>
+    </>
+  );
 }
 
 export default UseEffect;
